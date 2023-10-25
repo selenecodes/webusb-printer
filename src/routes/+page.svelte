17db347a966fd939dbe18d1643fcd9b94feb6fd3
Selene Blok <selene.blok@gmail.com>
2023-10-25 17:44:19 +0200

    feat: Made possible to input receipt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 
 <script lang="ts">
+	import { EXAMPLE_RECEIPT_1 } from "./example-receipts";
 	import { Printer } from "./printer";
 	let printer: Printer | undefined;
+	let text: string = EXAMPLE_RECEIPT_1;
 
 	async function setPrinter() {
 		printer = await Printer.setup();
@@ -12,28 +14,14 @@
 		
 	// 	`);
 	// }
-
-	const EXAMPLE_RECEIPT = `{image:iVBORw0KGgoAAAANSUhEUgAAAQAAAAA8AgMAAAD004yXAAAACVBMVEVwAJsAAAD///+esS7BAAAAAXRSTlMAQObYZgAAAZtJREFUSMftlkGOwyAMRW0J76kE97GlZu9KcP+rzCekKak6bWdGmrZS6KIG/7yAbQhEe+tN6qUpDZ1KPHT8SnhpeaP6FlA2wicBsgOeBpTF6gDfZHgj9Jt1sAMeAYYE/RFQngYMuX49gD8fMObjkwB+++h+DeB3x/qdvfD/AP4QwPXX+ceAUdV6Y7K/3Vr7rWhv73ZN9XVHzOUdlm6v9ay3pM1eLT+Ppv63BWjz8jJU0vpUWgGs5yfihrN451G+lq7i2bkF8Bagw3Qv0hEQKGTPPjnGxJtZrSIcJy5ciJ3JStbarjrgwOtVK7Z1iLFOOgOSR3WstTqMqCcVFlITDnhhNhMKzJNIA3iEBr1uaAfArA7bSWdAkRrIsJSMrwXbJBowEhogwe9F+NilC8D1oIyg43fA+1WwKpoktXUmsiOCjxFMR+gEBfwcaJ7qDBBWNTVSxKsZZhwkUgkppEiJ1VOIQdCFtAmNBQg9QWj9POQWFW4Bh0HVipRIHlpEUVzUAmslpgQpVcS0SklcrBqf03u/UvVhLd8H5Hffil/ia4Io3warBgAAAABJRU5ErkJggg==}
-
-Ichigaya Terminal
-1-Y-X Kudan, Chiyoda-ku
-02-07-2021 21:00
-{border:line; width:30}
-^RECEIPT
-{border:space; width:*,2,10}
-BEER                   | 2|     13.00
-CHIDORI                | 2|    172.80
--------------------------------------
-{width:*,20}
-^TOTAL             |          ^185.80
-CASH               |           200.00
-CHANGE             |            14.20
-{code:20210207210001; option:48,hri}`
 </script>
 
-<button on:click={setPrinter}>Request usb device</button>
-{#if printer?.printer}
-	<button on:click={() => printer?.print(EXAMPLE_RECEIPT)}>
-		Print
-	</button>
-{/if}
+<div style="display:grid; grid-auto-flow: row; gap: 1em;">
+	<button on:click={setPrinter}>Request usb device</button>
+	{#if printer}
+		<textarea rows="20" cols="80" bind:value={text}></textarea>
+		<button on:click={() => printer?.print(text)}>
+			Print
+		</button>
+	{/if}
+</div>
